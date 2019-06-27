c34208a784178319ea40b31cb4cdde1b4ddc42a6
tiramisu-zl <tiramisu101111@gmail.com>
2019-06-27 16:14:56 +0800

    rename property for LineItem

--- src/main/java/org/katas/refactoring/LineItem.java
+++ src/main/java/org/katas/refactoring/LineItem.java
@@ -2,14 +2,14 @@ package org.katas.refactoring;
 
 public class LineItem {
     private String desc;
-    private double p;
-    private int qty;
+    private double price;
+    private int quantity;
 
     public LineItem(String desc, double p, int qty) {
         super();
         this.desc = desc;
-        this.p = p;
-        this.qty = qty;
+        this.price = p;
+        this.quantity = qty;
     }
 
     public String getDescription() {
@@ -17,14 +17,14 @@ public class LineItem {
     }
 
     public double getPrice() {
-        return p;
+        return price;
     }
 
     public int getQuantity() {
-        return qty;
+        return quantity;
     }
 
     double totalAmount() {
-        return p * qty;
+        return price * quantity;
     }
 }
